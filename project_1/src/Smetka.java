f3452f7ab6f75893cbb7ad7d0732c20efef34d4e
blazeskibojan <148095416+blazeskibojan@users.noreply.github.com>
2023-10-16 17:20:37 +0200

    Add files via upload

---
+++ project_1/src/Smetka.java
@@ -0,0 +1,14 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y=32;
+		int x=20;
+		double b=50;
+		double rezultati=(x*2)+(y*5)+(b*2.5);
+		System.out.println("vkupno vo denari");
+		System.out.println(rezultati);
+	}
+
+}
